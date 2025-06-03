ec064141a9b0309a4d68482eb67b27dfec50a70a
PankovD <71269545+PankovD@users.noreply.github.com>
2025-06-03 12:07:30 +0300

    Update constants.py

--- core/constants.py
+++ core/constants.py
@@ -74,5 +74,97 @@ BASE_HEADERS = [
     "sec-fetch-user": "?1",
     "sec-fetch-dest": "document",
     "cookie": "consent=yes;"    
+    },
+    
+    {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-US,en;q=0.9,uk;q=0.8",
+    "accept-encoding": "gzip, deflate, br",
+    "referer": "https://www.walmart.com/",
+    "upgrade-insecure-requests": "1",
+    "dnt": "1",
+    "connection": "keep-alive",
+    "cache-control": "max-age=0",
+    "viewport-width": "1485",
+    "device-memory": "4",
+    "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"123\", \"Google Chrome\";v=\"123\"",
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-ch-ua-platform-version": "\"10.0.0\"",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-user": "?1",
+    "sec-fetch-dest": "document",
+    "cookie": "consent=yes;"
+    },
+    
+    {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-US,en;q=0.9,uk;q=0.8",
+    "accept-encoding": "gzip, deflate, br",
+    "referer": "https://www.walmart.com/",
+    "upgrade-insecure-requests": "1",
+    "dnt": "1",
+    "connection": "keep-alive",
+    "cache-control": "max-age=0",
+    "viewport-width": "1871",
+    "device-memory": "16",
+    "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"123\", \"Google Chrome\";v=\"123\"",
+    # sec-ch-ua-mobile видалено
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-ch-ua-platform-version": "\"10.0.0\"",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-user": "?1",
+    "sec-fetch-dest": "document"
+    # cookie видалено
+    },
+    
+    {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-US,en;q=0.9,uk;q=0.8",
+    "accept-encoding": "gzip, deflate, br",
+    "referer": "https://www.walmart.com/",
+    "upgrade-insecure-requests": "1",
+    "dnt": "1",
+    "connection": "keep-alive",
+    "cache-control": "max-age=0",
+    "viewport-width": "1866",
+    "device-memory": "8",
+    "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"123\", \"Google Chrome\";v=\"123\"",
+    "sec-ch-ua-mobile": "?1",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-ch-ua-platform-version": "\"10.0.0\"",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-user": "?1",
+    "sec-fetch-dest": "document"
+    # cookie не вказано
+    },
+    
+    {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-US,en;q=0.9,uk;q=0.8",
+    "accept-encoding": "gzip, deflate, br",
+    "referer": "https://www.walmart.com/",
+    "upgrade-insecure-requests": "1",
+    "dnt": "1",
+    "connection": "keep-alive",
+    "cache-control": "max-age=0",
+    "viewport-width": "1574",
+    "device-memory": "16",
+    "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"123\", \"Google Chrome\";v=\"123\"",
+    # sec-ch-ua-mobile видалено
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-ch-ua-platform-version": "\"10.0.0\"",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-user": "?1",
+    "sec-fetch-dest": "document",
+    "cookie": "consent=yes;"
     }
 ]
