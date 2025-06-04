886ad73a89d12a64e97f8a8973726ea3b68a3119
PankovD <71269545+PankovD@users.noreply.github.com>
2025-06-04 16:13:09 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -9,7 +9,7 @@ TOKEN_URL = "https://marketplace.walmartapis.com/v3/token"
 
 # Шляхи збереження
 CURRENT_TIME = time.strftime("%Y-%m-%d_%H-%M-%S")
-OUTPUT_FOLDER = "results"
+OUTPUT_FOLDER = "logging"
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 LOG_FILE = os.path.join(OUTPUT_FOLDER, f"log_{CURRENT_TIME}.txt")
 logging.basicConfig(
